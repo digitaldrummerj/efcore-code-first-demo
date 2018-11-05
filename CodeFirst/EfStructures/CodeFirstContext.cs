c50e20e26332ad7d102c29cc1cfe8de39fbc2c59
Justin James <digitaldrummerj@gmail.com>
2018-11-04 18:51:42 -0700

    add: indexes and migration

--- CodeFirst/EfStructures/CodeFirstContext.cs
+++ CodeFirst/EfStructures/CodeFirstContext.cs
@@ -13,5 +13,17 @@ namespace CodeFirst
         public DbSet<Car> Car { get; set; }
         public DbSet<Blog> Blog { get; set; }
         public DbSet<Post> Post { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasIndex(b => b.BlogUrl)
+                .IsUnique();
+
+            modelBuilder.Entity<Post>()
+                .HasIndex(b => b.Title);
+
+            modelBuilder.Entity<Car>().HasIndex(b => new {b.Make, b.Model});
+        }
     }
 }
---
+++ CodeFirst/EfStructures/Migrations/20181105014816_Indexes.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirst.EfStructures.Migrations
+{
+    public partial class Indexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Title",
+                table: "Posts",
+                column: "Title");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_Make_Model",
+                table: "Cars",
+                columns: new[] { "Make", "Model" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_BlogUrl",
+                table: "Blogs",
+                column: "BlogUrl",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_Title",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_Make_Model",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_BlogUrl",
+                table: "Blogs");
+        }
+    }
+}
