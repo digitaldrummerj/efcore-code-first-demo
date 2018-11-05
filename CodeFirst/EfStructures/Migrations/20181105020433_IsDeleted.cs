14058a27e1fee7a2d14a94899fcd6cf0b32b4f1d
Justin James <digitaldrummerj@gmail.com>
2018-11-04 19:06:27 -0700

    add: isDeleted migration

---
+++ CodeFirst/EfStructures/Migrations/20181105020433_IsDeleted.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirst.EfStructures.Migrations
+{
+    public partial class IsDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Blogs",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Blogs");
+        }
+    }
+}
