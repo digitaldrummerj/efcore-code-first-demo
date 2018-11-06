495c65122bfa52ce5956b13c16043631cb831eb4
Justin James <digitaldrummerj@gmail.com>
2018-11-05 19:49:34 -0700

    add: sql script for migration

---
+++ CodeFirst/EfCoreCodeFirst.sql
@@ -0,0 +1,374 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105003549_InitialCreate')
+BEGIN
+    CREATE TABLE [Blog] (
+        [Id] int NOT NULL IDENTITY,
+        [Created] datetimeoffset NOT NULL,
+        [Modified] datetimeoffset NOT NULL,
+        [BlogUrl] nvarchar(max) NULL,
+        [Name] nvarchar(max) NULL,
+        CONSTRAINT [PK_Blog] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105003549_InitialCreate')
+BEGIN
+    CREATE TABLE [Car] (
+        [Id] int NOT NULL IDENTITY,
+        [Created] datetimeoffset NOT NULL,
+        [Modified] datetimeoffset NOT NULL,
+        [Cylinders] int NOT NULL,
+        [Make] nvarchar(max) NULL,
+        [Model] nvarchar(max) NULL,
+        CONSTRAINT [PK_Car] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105003549_InitialCreate')
+BEGIN
+    CREATE TABLE [Post] (
+        [Id] int NOT NULL IDENTITY,
+        [Created] datetimeoffset NOT NULL,
+        [Modified] datetimeoffset NOT NULL,
+        [Title] nvarchar(max) NULL,
+        [Content] nvarchar(max) NULL,
+        [BlogId] int NOT NULL,
+        CONSTRAINT [PK_Post] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Post_Blog_BlogId] FOREIGN KEY ([BlogId]) REFERENCES [Blog] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105003549_InitialCreate')
+BEGIN
+    CREATE INDEX [IX_Post_BlogId] ON [Post] ([BlogId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105003549_InitialCreate')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181105003549_InitialCreate', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Post] DROP CONSTRAINT [FK_Post_Blog_BlogId];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Post] DROP CONSTRAINT [PK_Post];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Car] DROP CONSTRAINT [PK_Car];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Blog] DROP CONSTRAINT [PK_Blog];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    EXEC sp_rename N'[Post]', N'Posts';
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    EXEC sp_rename N'[Car]', N'Cars';
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    EXEC sp_rename N'[Blog]', N'Blogs';
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    EXEC sp_rename N'[Posts].[IX_Post_BlogId]', N'IX_Posts_BlogId', N'INDEX';
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Posts] ADD CONSTRAINT [PK_Posts] PRIMARY KEY ([Id]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Cars] ADD CONSTRAINT [PK_Cars] PRIMARY KEY ([Id]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Blogs] ADD CONSTRAINT [PK_Blogs] PRIMARY KEY ([Id]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    ALTER TABLE [Posts] ADD CONSTRAINT [FK_Posts_Blogs_BlogId] FOREIGN KEY ([BlogId]) REFERENCES [Blogs] ([Id]) ON DELETE CASCADE;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105005923_TableName')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181105005923_TableName', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105011631_ColumnRequiredMaxLength')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Posts]') AND [c].[name] = N'Title');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Posts] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Posts] ALTER COLUMN [Title] nvarchar(200) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105011631_ColumnRequiredMaxLength')
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Posts]') AND [c].[name] = N'Content');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Posts] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Posts] ALTER COLUMN [Content] nvarchar(max) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105011631_ColumnRequiredMaxLength')
+BEGIN
+    DECLARE @var2 sysname;
+    SELECT @var2 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Cars]') AND [c].[name] = N'Model');
+    IF @var2 IS NOT NULL EXEC(N'ALTER TABLE [Cars] DROP CONSTRAINT [' + @var2 + '];');
+    ALTER TABLE [Cars] ALTER COLUMN [Model] nvarchar(255) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105011631_ColumnRequiredMaxLength')
+BEGIN
+    DECLARE @var3 sysname;
+    SELECT @var3 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Cars]') AND [c].[name] = N'Make');
+    IF @var3 IS NOT NULL EXEC(N'ALTER TABLE [Cars] DROP CONSTRAINT [' + @var3 + '];');
+    ALTER TABLE [Cars] ALTER COLUMN [Make] nvarchar(100) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105011631_ColumnRequiredMaxLength')
+BEGIN
+    DECLARE @var4 sysname;
+    SELECT @var4 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Blogs]') AND [c].[name] = N'Name');
+    IF @var4 IS NOT NULL EXEC(N'ALTER TABLE [Blogs] DROP CONSTRAINT [' + @var4 + '];');
+    ALTER TABLE [Blogs] ALTER COLUMN [Name] nvarchar(100) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105011631_ColumnRequiredMaxLength')
+BEGIN
+    DECLARE @var5 sysname;
+    SELECT @var5 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Blogs]') AND [c].[name] = N'BlogUrl');
+    IF @var5 IS NOT NULL EXEC(N'ALTER TABLE [Blogs] DROP CONSTRAINT [' + @var5 + '];');
+    ALTER TABLE [Blogs] ALTER COLUMN [BlogUrl] nvarchar(255) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105011631_ColumnRequiredMaxLength')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181105011631_ColumnRequiredMaxLength', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105014038_Relationships')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181105014038_Relationships', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105014816_Indexes')
+BEGIN
+    CREATE INDEX [IX_Posts_Title] ON [Posts] ([Title]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105014816_Indexes')
+BEGIN
+    CREATE INDEX [IX_Cars_Make_Model] ON [Cars] ([Make], [Model]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105014816_Indexes')
+BEGIN
+    CREATE UNIQUE INDEX [IX_Blogs_BlogUrl] ON [Blogs] ([BlogUrl]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105014816_Indexes')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181105014816_Indexes', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105020433_IsDeleted')
+BEGIN
+    ALTER TABLE [Blogs] ADD [IsDeleted] bit NOT NULL DEFAULT 0;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105020433_IsDeleted')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181105020433_IsDeleted', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105021236_IsDeletedPost')
+BEGIN
+    ALTER TABLE [Posts] ADD [IsDeleted] bit NOT NULL DEFAULT 0;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181105021236_IsDeletedPost')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181105021236_IsDeletedPost', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181106021459_TurnOffCascade')
+BEGIN
+    ALTER TABLE [Posts] DROP CONSTRAINT [FK_Posts_Blogs_BlogId];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181106021459_TurnOffCascade')
+BEGIN
+    ALTER TABLE [Posts] ADD CONSTRAINT [FK_Posts_Blogs_BlogId] FOREIGN KEY ([BlogId]) REFERENCES [Blogs] ([Id]) ON DELETE NO ACTION;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181106021459_TurnOffCascade')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181106021459_TurnOffCascade', N'2.1.4-rtm-31024');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181106023218_DefaultValues')
+BEGIN
+    DECLARE @var6 sysname;
+    SELECT @var6 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Posts]') AND [c].[name] = N'Created');
+    IF @var6 IS NOT NULL EXEC(N'ALTER TABLE [Posts] DROP CONSTRAINT [' + @var6 + '];');
+    ALTER TABLE [Posts] ALTER COLUMN [Created] datetimeoffset NOT NULL;
+    ALTER TABLE [Posts] ADD DEFAULT (getutcdate()) FOR [Created];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181106023218_DefaultValues')
+BEGIN
+    DECLARE @var7 sysname;
+    SELECT @var7 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Blogs]') AND [c].[name] = N'Created');
+    IF @var7 IS NOT NULL EXEC(N'ALTER TABLE [Blogs] DROP CONSTRAINT [' + @var7 + '];');
+    ALTER TABLE [Blogs] ALTER COLUMN [Created] datetimeoffset NOT NULL;
+    ALTER TABLE [Blogs] ADD DEFAULT (getutcdate()) FOR [Created];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20181106023218_DefaultValues')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20181106023218_DefaultValues', N'2.1.4-rtm-31024');
+END;
+
+GO
+
