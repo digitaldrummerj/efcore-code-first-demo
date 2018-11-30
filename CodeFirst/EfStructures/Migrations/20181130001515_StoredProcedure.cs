2a8b4b79d82befce51f4d2e83e854eef14d8e556
Justin James <digitaldrummerj@gmail.com>
2018-11-29 17:19:12 -0700

    add: stored procedure example

---
+++ CodeFirst/EfStructures/Migrations/20181130001515_StoredProcedure.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirst.EfStructures.Migrations
+{
+    public partial class StoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            const string sp = @"CREATE PROCEDURE [dbo].[GetAllBlogs]
+AS 
+BEGIN
+    SET NOCOUNT ON;
+    select * from Blogs
+END";
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE [dbo].[GetAllBlogs]");
+        }
+    }
+}
